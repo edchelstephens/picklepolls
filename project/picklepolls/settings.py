038e30008059b105450c71fa1d174984fe57a09b
Edchel Stephen Nini <edchelstephens@gmail.com>
2026-07-30 17:50:09 +0800

    chore: templates.

--- project/picklepolls/settings.py
+++ project/picklepolls/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-ui7r8@z1y^geibe_d&ixx(q8zn!_4k-9-dos4dle4)bi$ve%(@"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -56,7 +56,7 @@ ROOT_URLCONF = "picklepolls.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
