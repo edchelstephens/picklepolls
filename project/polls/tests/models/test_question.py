88b5b49d3c7ff2c5103977885eac756bb6a2f154
Edchel Stephen Nini <edchelstephens@gmail.com>
2026-08-04 15:47:35 +0800

    chore: quesiton model test case.

--- project/polls/tests/models/test_question.py
+++ project/polls/tests/models/test_question.py
@@ -13,12 +13,23 @@ class QuestionModelTestCase(TestCase):
     def setUp(self) -> None:
         """Run this setUp before each test."""
         super().setUp()
-        self.future_datetime = timezone.now() + datetime.timedelta(days=30)
-        self.future_question = QuestionFactory(
-            publication_datetime=self.future_datetime
-        )
+
+        self.question = QuestionFactory()
 
     def test_was_published_recently_with_future_question(self) -> None:
         """Test was_published_recently() with future question should return False."""
 
-        self.assertFalse(self.future_question.was_published_recently())
+        future_datetime = timezone.now() + datetime.timedelta(days=30)
+        self.question.publication_datetime = future_datetime
+        self.question.save()
+        self.question.refresh_from_db()
+        self.assertFalse(self.question.was_published_recently())
+
+    def test_was_published_recently_with_old_question(self) -> None:
+        """Test was_published_recently() with old question should return False."""
+
+        old_datetime = timezone.now() - datetime.timedelta(days=30)
+        self.question.publication_datetime = old_datetime
+        self.question.save()
+        self.question.refresh_from_db()
+        self.assertFalse(self.question.was_published_recently())
