94d4b8dc85bbbead64f81d4cc8d5e13536cca2fa
Edchel Stephen Nini <edchelstephens@gmail.com>
2026-08-05 13:10:29 +0800

    test: test_total_choices_returns_total_count_of_choices.

--- project/polls/tests/models/test_question.py
+++ project/polls/tests/models/test_question.py
@@ -3,9 +3,11 @@ import datetime
 
 from django.utils import timezone
 
+from utils.tests.testcases import ModelTestCase
 
 from polls.tests.factories import QuestionFactory
-from utils.tests.testcases import ModelTestCase
+from polls.tests.factories import ChoiceFactory
+from polls.models import Choice
 
 
 class QuestionModelTestCase(ModelTestCase):
@@ -17,6 +19,12 @@ class QuestionModelTestCase(ModelTestCase):
 
         self.question_text = "3rd Short Drive or 3rd Shot Drop?"
         self.question = QuestionFactory(question_text=self.question_text)
+
+        self.choice_1_votes = 6
+        self.choice_1 = ChoiceFactory(question=self.question, votes=self.choice_1_votes)
+
+        self.choice_2_votes = 4
+        self.choice_2 = ChoiceFactory(question=self.question, votes=self.choice_2_votes)
 
     def test_str_method(self):
         """Test string method of Question model."""
@@ -49,3 +57,10 @@ class QuestionModelTestCase(ModelTestCase):
         self.question.save()
         self.question.refresh_from_db()
         self.assertFalse(self.question.was_published_recently())
+
+    def test_total_choices_returns_total_count_of_choices(self) -> None:
+        """Total chioces returns total amount of related Choice records."""
+        expected = Choice.objects.filter(question=self.question).count()
+        actual = self.question.total_choices
+
+        self.assertEqual(actual, expected)
