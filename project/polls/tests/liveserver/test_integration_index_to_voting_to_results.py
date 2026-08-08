23b8690c8bf9fdf53e0ece2130bdd9a857bfc4f3
Edchel Stephen Nini <edchelstephens@gmail.com>
2026-08-08 15:53:16 +0800

    chore: install firefox on the machine.

--- project/polls/tests/liveserver/test_integration_index_to_voting_to_results.py
+++ project/polls/tests/liveserver/test_integration_index_to_voting_to_results.py
@@ -11,7 +11,6 @@ from polls.models import Choice, Question
 from accounts.tests.factories.entity import SaturdayLateNightPickleballEntityFactory
 
 
-@pytest.mark.skip_on_ci
 @pytest.mark.liveserver
 class IntegrationTestIndexToVotingToResultsSeleniumTestCase(
     DjangoStaticLiveServerTestCase
