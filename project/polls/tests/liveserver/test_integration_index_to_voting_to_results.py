0dc3fb45bfad21838588d20940e0f827bf4d25d5
Edchel Stephen Nini <edchelstephens@gmail.com>
2026-08-11 12:08:59 +0800

    chore: test_voting_on_an_choice_updates_the_poll_results.

--- project/polls/tests/liveserver/test_integration_index_to_voting_to_results.py
+++ project/polls/tests/liveserver/test_integration_index_to_voting_to_results.py
@@ -14,6 +14,7 @@ from accounts.tests.factories.entity import SaturdayLateNightPickleballEntityFac
 from utils.testing_utils.liveserver_testcases import DjangoStaticLiveServerTestCase
 
 
+@pytest.mark.solo
 @pytest.mark.liveserver
 class IntegrationTestIndexToVotingToResultsSeleniumTestCase(
     DjangoStaticLiveServerTestCase
@@ -55,6 +56,11 @@ class IntegrationTestIndexToVotingToResultsSeleniumTestCase(
         )
         self.votes_before = sum(Choice.objects.values_list("votes", flat=True))
 
+    def pluralize(self, count: int, text: str) -> str:
+        """Pluralize text based on count int value."""
+
+        return text + "s" if count != 1 else text
+
     def test_voting_on_an_choice_updates_the_poll_results(
         self,
     ) -> None:
@@ -95,3 +101,72 @@ class IntegrationTestIndexToVotingToResultsSeleniumTestCase(
 
         self.assertIn(expected_text, self.selenium.page_source)
         self.assertEqual(expected_votes, votes_after)
+
+        winning_choice_p_element = self.selenium.find_element(
+            By.ID, "winning-choice-text"
+        )
+
+        expected_winning_choice_p_element_text = self.choice_1.choice_text + " 🎉"
+        winning_choice_span_element_on_breakdown = self.selenium.find_element(
+            By.ID, "winning-choice-text-on-breakdown"
+        )
+
+        winning_choice_votes_on_breakdown_span_element = self.selenium.find_element(
+            By.ID, "winning-choice-votes-on-breakdown"
+        )
+
+        expected_winning_votes_text = f"{self.choice_1.votes} {self.pluralize(count=self.choice_1.votes, text='vote')}"
+
+        self.assertEqual(
+            winning_choice_p_element.text, expected_winning_choice_p_element_text
+        )
+        self.assertEqual(
+            winning_choice_span_element_on_breakdown.text, self.choice_1.choice_text
+        )
+
+        self.assertEqual(
+            winning_choice_votes_on_breakdown_span_element.text,
+            expected_winning_votes_text,
+        )
+
+    # @pytest.mark.skip
+    # def test_all_links_are_working(
+    #     self,
+    # ) -> None:
+    #     """Test that all links are working."""
+
+    #     self.selenium.get(f"{self.live_server_url}/")
+
+    #     self.pause(seconds=2)
+
+    #     vote_link_id = f"vote-on-poll-{self.question_1.pk}"
+    #     vote_link = self.selenium.find_element(By.ID, vote_link_id)
+    #     vote_link.click()
+
+    #     self.pause(seconds=2)
+
+    #     choice_radio_button_id = f"choice-{self.choice_1.pk}"
+    #     choice_button = self.selenium.find_element(By.ID, choice_radio_button_id)
+    #     choice_button.click()
+
+    #     self.pause(seconds=2)
+
+    #     submit_button = self.selenium.find_element(
+    #         By.CSS_SELECTOR, 'button[type="submit"]'
+    #     )
+    #     submit_button.click()
+    #     self.pause(seconds=2)
+
+    #     votes_after = sum(Choice.objects.values_list("votes", flat=True))
+
+    #     self.choice_1.refresh_from_db()
+    #     choice_1_votes_after = int(self.choice_1.votes)
+
+    #     self.assertGreater(choice_1_votes_after, self.choice_1_initial_votes)
+    #     self.assertEqual(self.choice_1_initial_votes + 1, choice_1_votes_after)
+
+    #     expected_votes = self.choice_1_initial_votes + self.choice_2_initial_votes + 1
+    #     expected_text = f"{expected_votes} total responses logged"
+
+    #     self.assertIn(expected_text, self.selenium.page_source)
+    #     self.assertEqual(expected_votes, votes_after)
