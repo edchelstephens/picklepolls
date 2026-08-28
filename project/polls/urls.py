bbb73bf5fa800bd4c8b269dbca2bb94a37060278
Edchel Stephen Nini <edchelstephens@gmail.com>
2026-08-28 13:36:09 +0800

    chore: fix urls.

--- project/polls/urls.py
+++ project/polls/urls.py
@@ -7,7 +7,6 @@ app_name = "polls"
 urlpatterns = [
     path("", views.PollsIndexView.as_view(), name="index"),
     path("polls/<int:pk>/", views.PollDetailView.as_view(), name="detail"),
-    path("pols/<int:pk>/results/", views.PollResultsView.as_view(), name="results"),
+    path("polls/<int:pk>/results/", views.PollResultsView.as_view(), name="results"),
     path("polls/<int:pk>/vote/", views.PollVoteView.as_view(), name="vote"),
-    
 ]
