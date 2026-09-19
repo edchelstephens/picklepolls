7338c535fbe0f26cb2d65e9337895086598eb5cb
Edchel Stephen Nini <edchelstephens@gmail.com>
2026-09-19 12:34:52 +0800

    fix: add open telemetry for tracing and explicitly declare DJANGO_SETTINGS_MODULE so it can be found when opentelemetry auto instruments django.

--- entrypoint.local.sh
+++ entrypoint.local.sh
@@ -7,4 +7,4 @@ python manage.py migrate --noinput
 
 echo "Starting Django development server..."
 
-opentelemetry-instrument python manage.py runserver 0.0.0.0:8000
+exec opentelemetry-instrument python manage.py runserver 0.0.0.0:8000
